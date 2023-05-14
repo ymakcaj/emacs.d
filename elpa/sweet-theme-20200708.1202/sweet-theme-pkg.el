af4fe1d63a8def0d8cd7559103992a996038a17a
Jack Amy <jack.amy@moneyfarm.com>
2023-05-14 22:23:43 +0100

    latest version

---
+++ elpa/sweet-theme-20200708.1202/sweet-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from sweet-theme.el  -*- no-byte-compile: t -*-
+(define-package "sweet-theme" "20200708.1202" "Sweet-looking theme" '((emacs "24.1")) :commit "ccbfdb6a17e25ab18a0b64101675bc1dfef44006" :authors '(("2bruh4me")) :maintainers '(("2bruh4me")) :maintainer '("2bruh4me") :keywords '("faces") :url "https://github.com/2bruh4me/sweet-theme")
--- init.el
+++ init.el
@@ -4,88 +4,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(connection-local-criteria-alist
-   '(((:application tramp :protocol "flatpak")
-      tramp-container-connection-local-default-flatpak-profile)
-     ((:application tramp)
-      tramp-connection-local-default-system-profile tramp-connection-local-default-shell-profile)))
- '(connection-local-profile-alist
-   '((tramp-container-connection-local-default-flatpak-profile
-      (tramp-remote-path "/app/bin" tramp-default-remote-path "/bin" "/usr/bin" "/sbin" "/usr/sbin" "/usr/local/bin" "/usr/local/sbin" "/local/bin" "/local/freeware/bin" "/local/gnu/bin" "/usr/freeware/bin" "/usr/pkg/bin" "/usr/contrib/bin" "/opt/bin" "/opt/sbin" "/opt/local/bin"))
-     (tramp-connection-local-darwin-ps-profile
-      (tramp-process-attributes-ps-args "-acxww" "-o" "pid,uid,user,gid,comm=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" "-o" "state=abcde" "-o" "ppid,pgid,sess,tty,tpgid,minflt,majflt,time,pri,nice,vsz,rss,etime,pcpu,pmem,args")
-      (tramp-process-attributes-ps-format
-       (pid . number)
-       (euid . number)
-       (user . string)
-       (egid . number)
-       (comm . 52)
-       (state . 5)
-       (ppid . number)
-       (pgrp . number)
-       (sess . number)
-       (ttname . string)
-       (tpgid . number)
-       (minflt . number)
-       (majflt . number)
-       (time . tramp-ps-time)
-       (pri . number)
-       (nice . number)
-       (vsize . number)
-       (rss . number)
-       (etime . tramp-ps-time)
-       (pcpu . number)
-       (pmem . number)
-       (args)))
-     (tramp-connection-local-busybox-ps-profile
-      (tramp-process-attributes-ps-args "-o" "pid,user,group,comm=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" "-o" "stat=abcde" "-o" "ppid,pgid,tty,time,nice,etime,args")
-      (tramp-process-attributes-ps-format
-       (pid . number)
-       (user . string)
-       (group . string)
-       (comm . 52)
-       (state . 5)
-       (ppid . number)
-       (pgrp . number)
-       (ttname . string)
-       (time . tramp-ps-time)
-       (nice . number)
-       (etime . tramp-ps-time)
-       (args)))
-     (tramp-connection-local-bsd-ps-profile
-      (tramp-process-attributes-ps-args "-acxww" "-o" "pid,euid,user,egid,egroup,comm=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" "-o" "state,ppid,pgid,sid,tty,tpgid,minflt,majflt,time,pri,nice,vsz,rss,etimes,pcpu,pmem,args")
-      (tramp-process-attributes-ps-format
-       (pid . number)
-       (euid . number)
-       (user . string)
-       (egid . number)
-       (group . string)
-       (comm . 52)
-       (state . string)
-       (ppid . number)
-       (pgrp . number)
-       (sess . number)
-       (ttname . string)
-       (tpgid . number)
-       (minflt . number)
-       (majflt . number)
-       (time . tramp-ps-time)
-       (pri . number)
-       (nice . number)
-       (vsize . number)
-       (rss . number)
-       (etime . number)
-       (pcpu . number)
-       (pmem . number)
-       (args)))
-     (tramp-connection-local-default-shell-profile
-      (shell-file-name . "/bin/sh")
-      (shell-command-switch . "-c"))
-     (tramp-connection-local-default-system-profile
-      (path-separator . ":")
-      (null-device . "/dev/null"))))
  '(package-selected-packages
-   '(avy ## lsp-java lsp-ui pyenv-mode doom-themes ace-window bind-key ein eink-theme)))
+   '(company helm projectile yasnippet sweet-theme avy ## lsp-java lsp-ui pyenv-mode doom-themes ace-window bind-key ein eink-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -93,14 +13,87 @@
  ;; If there is more than one, they won't work right.
  )
 
-(require 'package)
-'' (require 'ein)
-'' (require 'ein-notebook)
-'' (require 'ein-subpackages)
-(add-to-list 'package-archives '("gnu"   . "https://elpa.gnu.org/packages/"))
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
-(package-refresh-contents)
-(package-initialize)
+ ;;; Settings without corresponding packages
+
+(use-package emacs
+  :preface
+  (defvar jack/indent-width 4)
+  (defun jack/maybe-set-default-browser ()
+    "When in WSL Emacs, open links in default Windows 11 browser."
+    (cond
+     ((eq system-type 'gnu/linux)
+      (when (string-match "Linux.*microsoft.*Linux"
+                          (shell-command-to-string "uname -a"))
+        (setq browse-url-generic-program "/mnt/c/Windows/System32/cmd.exe"
+              browse-url-generic-args '("/c" "start" "")
+              browse-url-browser-function 'browse-url-generic)))))
+  (defun jack/edit-config ()
+    "Open Emacs settings."
+    (interactive)
+    (find-file (concat user-emacs-directory "init.el")))
+  (defun jack/split-and-follow-horizontally ()
+    "Split window below."
+    (interactive)
+    (split-window-below)
+    (other-window 1))
+  (defun jack/split-and-follow-vertically ()
+    "Split window right."
+    (interactive)
+    (split-window-right)
+    (other-window 1))
+  :config
+  (setq user-full-name "Jack A.")
+  (setq frame-title-format '("Emacs " emacs-version))
+  (setq python-python-command "C:/Users/jack.amy/anaconda3/python.exe")
+  (setq-default default-directory "~/")
+  (setq frame-resize-pixelwise t)
+  (setq hscroll-margin 0)
+  (setq load-prefer-newer t)
+  (setq inhibit-compacting-font-caches t)
+  (setq echo-keystrokes 0.02)
+  (setq delete-by-moving-to-trash t)
+  (put 'downcase-region 'disabled nil)
+  (put 'upcase-region 'disabled nil)
+  (put 'scroll-right 'disabled nil)
+  (put 'scroll-left 'disabled nil)
+  (global-set-key (kbd "C-x 2") #'jack/split-and-follow-horizontally)
+  (global-set-key (kbd "C-x 3") #'jack/split-and-follow-vertically)
+  (global-set-key (kbd "C-s")   #'save-buffer)
+  (unless (display-graphic-p)
+    (global-set-key (kbd "C-h") #'backward-kill-word))
+  (setq-default indent-tabs-mode nil)
+  (setq-default tab-width 4)
+  (setq initial-scratch-message "")
+  (setq split-width-threshold 150)
+  (setq max-specpdl-size 10000)
+  (setq max-lisp-eval-depth 10000)
+  (set-default 'truncate-lines t)
+  (jack/maybe-set-default-browser)
+  (setq jit-lock-defer-time 0)
+  (setq fast-but-imprecise-scrolling t)
+  (xterm-mouse-mode +1)
+  (bind-key* (kbd "C-,") #'jack/edit-config)
+  (bind-key* (kbd "<f4>") #'(lambda ()
+                              (interactive)
+                              (if (one-window-p)
+                                  (kill-current-buffer)
+                                (kill-buffer-and-window)))))
+
+
+(condition-case nil
+    (require 'use-package)
+  (file-error
+   (require 'package)
+   (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+   (add-to-list 'package-archives '("gnu"   . "https://elpa.gnu.org/packages/"))
+   (package-initialize)
+   (package-refresh-contents)
+   (package-install 'use-package)
+   (setq use-package-always-ensure t)
+   (require 'use-package)))
+
+;;(require 'ein)
+;;(require 'ein-notebook)
 
 (use-package ace-window
   :ensure t)
@@ -108,157 +101,38 @@
 (require 'cl)
 (require 'color)
 
-(defun csv-highlight (&optional separator)
-  (interactive (list (when current-prefix-arg (read-char "Separator: "))))
-  (font-lock-mode 1)
-  (let* ((separator (or separator ?\,))
-         (n (count-matches (string separator) (point-at-bol) (point-at-eol)))
-         (colors (loop for i from 0 to 1.0 by (/ 2.0 n)
-                       collect (apply #'color-rgb-to-hex 
-                                      (color-hsl-to-rgb i 0.3 0.5)))))
-    (loop for i from 2 to n by 2 
-          for c in colors
-          for r = (format "^\\([^%c\n]+%c\\)\\{%d\\}" separator separator i)
-          do (font-lock-add-keywords nil `((,r (1 '(face (:foreground ,c)))))))))
-
-
-(add-hook 'csv-mode-hook 'csv-align-mode)
-(add-hook 'csv-mode-hook '(lambda () (interactive) (toggle-truncate-lines nil)))
 
 
 
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-
-(require 'use-package)
-
-;; (use-package lsp-pyright
-;;   :ensure t
-;;   :hook (python-mode . (lambda ()
-;; 			 (require 'lsp-pyright)
-;; 			 (lsp)))) ; or lsp-deferred
-
-(use-package doom-themes
-  :ensure t
-  :config
+;(use-package doom-themes
+ ; :ensure t
+  ;:config
   ;; glob settings (defaults)
-  (setq doom-themes-enable-bold t
-	doom-themes-enable-italic t)
-  (load-theme 'doom-one t)
+  ;(setq doom-themes-enable-bold t
+	;doom-themes-enable-italic t)
+  ;(load-theme 'doom-one t)
 
   ;; to enable flashing mode for errors
-  (doom-themes-visual-bell-config)
+  ;(doom-themes-visual-bell-config)
   ;; to enable neo-tree formate
-  (doom-themes-neotree-config))
+  ;(doom-themes-neotree-config))
   
-(unless (package-installed-p 'ein)
-  (package-refresh-contents)
-  (package-install 'ein))
-(eval-and-compile
-  (setq use-package-always-ensure t
-       use-package-expand-minimally t))
-
-(setq user-full-name "Jack A")
-(setq frame-title-format '("Emacs " emacs-version))
-(setq python-python-command "C:/Users/jack.amy/anaconda3/python.exe")
-
-(use-package lsp-ui
-  :commands lsp-ui-mode
-  :custom-face
-  (lsp-ui-sideline-global ((t (:italic t))))
-  (lsp-ui-peek-highlight  ((t (:foreground unspecified :background unspecified :inherit isearch))))
-  :config
-  (with-eval-after-load 'evil
-    (add-hook 'buffer-list-update-hook
-              #'(lambda ()
-                  (when (bound-and-true-p lsp-ui-mode)
-                    (evil-define-key '(motion normal) 'local (kbd "K")
-                      #'(lambda () (interactive) (lsp-ui-doc-glance) (ian/pulse-line)))))))
-  (setq lsp-ui-doc-enable nil)
-  (setq lsp-ui-doc-show-with-mouse nil)
-  (setq lsp-ui-doc-enhanced-markdown nil)
-  (setq lsp-ui-doc-delay 0.01)
-  (when (display-graphic-p)
-    (setq lsp-ui-doc-use-childframe t)
-    (setq lsp-ui-doc-text-scale-level -1.0)
-    (setq lsp-ui-doc-max-width 80)
-    (setq lsp-ui-doc-max-height 25)
-    (setq lsp-ui-doc-position 'at-point))
-  (setq lsp-ui-doc-include-signature t)
-  (setq lsp-ui-doc-border (face-foreground 'font-lock-comment-face))
-  (setq lsp-ui-sideline-diagnostic-max-line-length 80)
-  (setq lsp-ui-sideline-diagnostic-max-lines 2)
-  (setq lsp-ui-peek-always-show t)
-  (setq lsp-ui-sideline-delay 0.05))
-
-(use-package lsp-java
-  :after lsp)
-
-(use-package java
-  :ensure nil
-  :after lsp-java
-  :bind (:map java-mode-map ("C-c i" . lsp-java-add-import)))
-
-(use-package lsp-mode
-  :init
-  (setq lsp-keymap-prefix "C-c l")
-  :hook ((c-mode          ; clangd
-          c++-mode        ; clangd
-          c-or-c++-mode   ; clangd
-          ;; java-mode       ; eclipse-jdtls
-          js-mode         ; ts-ls (tsserver wrapper)
-          js-jsx-mode     ; ts-ls (tsserver wrapper)
-          typescript-mode ; ts-ls (tsserver wrapper)
-          python-mode     ; pyright
-          web-mode        ; ts-ls/HTML/CSS
-          rust-mode       ; rust-analyzer
-          go-mode         ; gopls
-          ) . lsp-deferred)
-  :preface
-  (defun ian/lsp-execute-code-action ()
-    "Execute code action with pulse-line animation."
-    (interactive)
-    (ian/pulse-line)
-    (call-interactively 'lsp-execute-code-action))
-  :custom-face
-  (lsp-headerline-breadcrumb-symbols-face                ((t (:inherit variable-pitch))))
-  (lsp-headerline-breadcrumb-path-face                   ((t (:inherit variable-pitch))))
-  (lsp-headerline-breadcrumb-project-prefix-face         ((t (:inherit variable-pitch))))
-  (lsp-headerline-breadcrumb-unknown-project-prefix-face ((t (:inherit variable-pitch))))
-  :commands lsp
-  :config
-  (add-hook 'java-mode-hook #'(lambda () (when (eq major-mode 'java-mode) (lsp-deferred))))
-  (global-unset-key (kbd "<f2>"))
-  (define-key lsp-mode-map (kbd "<f2>") #'lsp-rename)
-  (setq lsp-auto-guess-root t)
-  (setq lsp-log-io nil)
-  (setq lsp-restart 'auto-restart)
-  (setq lsp-enable-links nil)
-  (setq lsp-enable-symbol-highlighting nil)
-  (setq lsp-enable-on-type-formatting nil)
-  (setq lsp-lens-enable nil)
-  (setq lsp-signature-auto-activate nil)
-  (setq lsp-signature-render-documentation nil)
-  (setq lsp-eldoc-enable-hover nil)
-  (setq lsp-eldoc-hook nil)
-  (setq lsp-modeline-code-actions-enable nil)
-  (setq lsp-modeline-diagnostics-enable nil)
-  (setq lsp-headerline-breadcrumb-enable nil)
-  (setq lsp-headerline-breadcrumb-icons-enable nil)
-  (setq lsp-semantic-tokens-enable nil)
-  (setq lsp-enable-folding nil)
-  (setq lsp-enable-imenu nil)
-  (setq lsp-enable-snippet nil)
-  (setq lsp-enable-file-watchers nil)
-  (setq lsp-keep-workspace-alive nil)
-  (setq read-process-output-max (* 1024 1024)) ;; 1MB
-  (setq lsp-idle-delay 0.25)
-  (setq lsp-auto-execute-action nil)
-  (with-eval-after-load 'lsp-clangd
-    (setq lsp-clients-clangd-args '("--header-insertion=never" "-j=4" "-background-index")))
-  (add-to-list 'lsp-language-id-configuration '(js-jsx-mode . "javascriptreact")))
-
+(use-package projectile)
+(use-package flycheck)
+(use-package yasnippet :config (yas-global-mode))
+(use-package lsp-mode :hook ((lsp-mode . lsp-enable-which-key-integration))
+  :config (setq lsp-completion-enable-additional-text-edit nil))
+(use-package hydra)
+(use-package company)
+(use-package lsp-ui)
+(use-package which-key :config (which-key-mode))
+(use-package lsp-java :config (add-hook 'java-mode-hook 'lsp))
+(use-package dap-mode :after lsp-mode :config (dap-auto-configure-mode))
+(use-package dap-java :ensure nil)
+(use-package helm-lsp)
+(use-package helm
+  :config (helm-mode))
+(use-package lsp-treemacs)
 
 ;; (setenv "WORKON_HOME" "C:/Users/jack.amy/anaconda3") ; /anaconda3 || /miniconda || wathever path your conda installation is located at
 ;; (pyvenv-mode 1)
